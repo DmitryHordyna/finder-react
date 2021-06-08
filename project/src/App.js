bad8a1f0deb02d5c238d35f8bfdabbc494f5fab1
DmitryHordyna <71545910+DmitryHordyna@users.noreply.github.com>
2021-06-08 15:39:22 +0200

    add

--- project/src/App.js
+++ project/src/App.js
@@ -4,12 +4,44 @@ import API from './services/image-api';
 import axios from 'axios';
 // style
 
-import './App.scss';
+// import './App.scss'
 
-const BASE_URL = 'https://restcountries.eu/rest/v2';
+// const BASE_URL = 'https://restcountries.eu/rest/v2';
+
 class App extends Component {
+  state = {
+    searchQuery: 'toys',
+    currentPage: 1,
+    perPage: 12,
+    imageType: 'photo',
+    orientation: 'horizontal',
+  };
+
   componentDidMount() {
     fetch(
-      `https://pixabay.com/api/videos/?key=21266258-ed1f1a426d97c1dcb24ced78e&q=yellow+flowers'`,
+      `https://pixabay.com/api/?q=${this.state.searchQuery}&page=${this.state.currentPage}&key=21266258-ed1f1a426d97c1dcb24ced78e&image_type=${this.state.imageType}&orientation=${this.state.orientation}&per_page=${this.state.perPage}`,
     )
-      .then(response => { 43
+      .then(response => {
+        if (!response) {
+          throw response;
+        }
+        return response.json();
+      })
+      .then(response => console.log(response.hits));
+  }
+  render() {
+    return (
+      <>
+        <h1>Hello</h1>
+      </>
+    );
+  }
+}
+
+export default App;
+// return fetch(`${BASE_URL}/name/${querySelector}`).then(response => {
+//   if (!response.ok) {
+//     throw response;
+//   }
+//   return response.json();
+// });
